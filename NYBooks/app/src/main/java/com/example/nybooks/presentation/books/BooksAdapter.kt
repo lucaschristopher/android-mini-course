6b97d593ed41127195ca5f89db3d33b9b449821f
Lucas Christopher <lucas.christopher.silva@ccc.ufcg.edu.br>
2021-03-29 00:45:38 -0300

    wip: organizing the project and creating the presentation layer

---
+++ NYBooks/app/src/main/java/com/example/nybooks/presentation/books/BooksAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.nybooks.presentation.books
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.nybooks.R
+import com.example.nybooks.data.model.Book
+import kotlinx.android.synthetic.main.item_book.view.*
+
+class BooksAdapter(private val books: List<Book>) :
+    RecyclerView.Adapter<BooksAdapter.BooksViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BooksViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_book, parent, false)
+        return BooksViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: BooksViewHolder, position: Int) {
+        holder.bindViewHolder(books[position])
+    }
+
+    override fun getItemCount() = books.count()
+
+    class BooksViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private val title = view.textTitle
+        private val author = view.textAuthor
+
+        fun bindViewHolder(book: Book) {
+            title.text = book.title
+            author.text = book.author
+        }
+    }
+}
+
